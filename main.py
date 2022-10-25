702d24c989ac6dac98cce9ddb89c72ed1c7c8704
Pawel <wydrapawel@icloud.com>
2022-10-25 18:02:11 +0200

    add logic and style.Api request exception handle.

--- main.py
+++ main.py
@@ -13,18 +13,30 @@ class NewCafe(FlaskForm):
 app = Flask(__name__)
 Bootstrap(app)
 
-
 @app.route("/", methods=["POST", "GET"])
 def home():
     is_edit = False
+    definition = False
+    image_url = False
+    word = False
     form = NewCafe(meta={'csrf': False})
     if form.validate_on_submit():
-        pass
-        ### API Call ###
-        ### word = request.form.get("name")###
-        ### api return json with 3 values, last is nested dict with 5 k,v ###
+        import requests
+        headers = {
+            "Authorization": "Token 66ab3a3a00d3ff67dc360abaa73ff615cc5af22c"
+        }
+        word = request.form.get('name').capitalize()
+        try:
+            response = requests.get(f"https://owlbot.info/api/v4/dictionary/{word}", headers=headers)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            raise SystemExit(err)
+        data = response.json()
+        definition = data["definitions"][0]["definition"]
+        image_url = data["definitions"][0]["image_url"]
+
         is_edit = True
-    return render_template("index.html", form=form, is_edit=is_edit)
+    return render_template("index.html", form=form, is_edit=is_edit, definition=definition, image_url=image_url, word=word)
 
 
 if __name__ == '__main__':
